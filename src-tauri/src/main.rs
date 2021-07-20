0259fedd3a393b82093d3822fb4ff17ff5fd5cec
Yassen Damyanov <yassen@qredo.com>
2021-07-20 18:38:06 +0300

    Add simple 'user-click' event pub/sub schema

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,9 +10,35 @@ fn say_hello(name: String) -> String {
    response.into()
 }
 
+use tauri::Manager;
+
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+   message: String,
+}
+
 fn main() {
    println!("** Tauri app about to start");
    tauri::Builder::default()
+      .setup(|app| {
+         // listen to the `event-name` (emitted on any window)
+         let id = app.listen_global("user-click", |event| {
+            println!("got event with payload {:?}", event.payload());
+         });
+         // unlisten to the event using the `id` returned on the `listen_global` function
+         // an `once_global` API is also exposed on the `App` struct
+         app.unlisten(id);
+
+         // emit the `event-name` event to all webview windows on the frontend
+         app.emit_all(
+            "user-click",
+            Payload {
+               message: "Tauri is awesome!".into(),
+            },
+         )
+         .unwrap();
+         Ok(())
+      })
       .invoke_handler(tauri::generate_handler![say_hello,])
       .run(tauri::generate_context!())
       .expect("error while running tauri application");
